2759d7a02999df3ac4b039f290cf8f8691e642ff
K4l3b <143396031+CalebDevX@users.noreply.github.com>
2026-03-25 05:32:15 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,11 @@ import type { ReactNode } from 'react';
 import Script from 'next/script';
 
 export const metadata = {
-  title: 'Zipora Downloads – Cracked Apps, Software and File Downloads',
+  title: 'Zipora Downloads – Apps, Software and File Downloads',
   description:
-    'Discover and download activated APKs, EXEs, ZIPs, PDFs and more with a clean interface.',
-
+    'Discover and download APKs, EXEs, ZIPs, PDFs and more with a clean interface.',
   icons: {
     icon: '/favicon.png',
-  },
-
-  other: {
-    monetag: 'cd76fadb010bf0bacf74e000aec660b7',
   },
 };
 
@@ -26,53 +21,42 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body className="antialiased">
         {children}
 
-        {/* Monetag Scripts */}
-
+        {/* 🔥 POPUNDER / ONCLICK ADS */}
         <Script
-          src="https://5gvci.com/act/files/tag.min.js?z=10780556"
+          src="https://pl28974290.profitablecpmratenetwork.com/14/c1/19/14c1196c555973ee0ccabf1a947ef139.js"
           strategy="afterInteractive"
         />
 
-        <Script id="monetag-2" strategy="afterInteractive">
+        <Script
+          src="https://pl28974324.profitablecpmratenetwork.com/84/fc/29/84fc29951728fe1441899ffa701061b1.js"
+          strategy="afterInteractive"
+        />
+
+        {/* 🔥 BANNER (AUTO CONTAINER) */}
+        <Script
+          src="https://pl28974291.profitablecpmratenetwork.com/a3b45e7b268818c756409a3268cece57/invoke.js"
+          strategy="afterInteractive"
+        />
+        
+        <div id="container-a3b45e7b268818c756409a3268cece57"></div>
+
+        {/* 🔥 300x250 IFRAME BANNER */}
+        <Script id="ad-options" strategy="afterInteractive">
           {`
-            (function(s){
-              s.dataset.zone='10780566';
-              s.src='https://nap5k.com/tag.min.js';
-            })(document.body.appendChild(document.createElement('script')));
+            var atOptions = {
+              key: 'c5aaf83badd7f4b369cf4d2497fdd48d',
+              format: 'iframe',
+              height: 250,
+              width: 300,
+              params: {}
+            };
           `}
         </Script>
 
-        <Script id="monetag-3" strategy="afterInteractive">
-          {`
-            (function(s){
-              s.dataset.zone='10780568';
-              s.src='https://izcle.com/vignette.min.js';
-            })(document.body.appendChild(document.createElement('script')));
-          `}
-        </Script>
-
-      </body>
-    </html>
-  );
-}
-            })([document.documentElement, document.body]
-            .filter(Boolean)
-            .pop()
-            .appendChild(document.createElement('script')));
-          `}
-        </Script>
-
-        <Script id="monetag-3" strategy="afterInteractive">
-          {`
-            (function(s){
-              s.dataset.zone='10780568',
-              s.src='https://izcle.com/vignette.min.js'
-            })([document.documentElement, document.body]
-            .filter(Boolean)
-            .pop()
-            .appendChild(document.createElement('script')));
-          `}
-        </Script>
+        <Script
+          src="https://www.highperformanceformat.com/c5aaf83badd7f4b369cf4d2497fdd48d/invoke.js"
+          strategy="afterInteractive"
+        />
 
       </body>
     </html>
